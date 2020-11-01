551e49e5da11945465dee238d80497e03fb61a47
Kavosh Madadi <kavoshmadadiglk@gmail.com>
2020-11-01 19:00:16 +0330

    User Interface
    some new changes in UI. put screen and buttons into the separated frames.

--- calculator.py
+++ calculator.py
@@ -3,8 +3,8 @@ from tkinter import *
 root = Tk()
 root.title('A Simple Calculator')
 #root.geometry('500x600')
-width=524
-height=366
+width=490
+height=390
 screenwidth = root.winfo_screenwidth()
 screenheight = root.winfo_screenheight()
 alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
@@ -15,9 +15,26 @@ root.resizable(width=False, height=False)
 isPushed = StringVar()
 #print(isPushed.get() == '')
 var = StringVar()
-#screenFrame = Frame(root)
-screenLabel = Label(root, text='', width=20, height=3,padx=150, pady=13, bg='#ffe6e6',
-                    fg='#332600', font=('Times', 15), textvariable=var)
+screenFrame = Frame(root, width=32, height=20, bd=4, relief=SUNKEN)
+screenFrame.pack(fill=X, padx=10, pady=4)
+
+buttonFrame = Frame(root, width=32, height=40, bd=4, relief=SUNKEN)
+buttonFrame.pack(fill=X, padx=10, pady=4)
+
+
+screenLabel = Label(
+	screenFrame,
+	text='',
+	padx=1,
+	pady=1,
+	width=20,
+	height=2,
+	bg='#ffe6e6',
+	fg='#332600',
+	font=('Times', 20),
+	anchor=E,
+	textvariable=var
+)
 
 #define commands
 #numbers
@@ -109,7 +126,7 @@ def add():
     isPushed.set('')
 def subtract():
     s = var.get()
-    var.set(' {} - '.format(s))
+    var.set(' {} − '.format(s))
     isPushed.set('')
 def multiply():
     s = var.get()
@@ -132,13 +149,13 @@ def calculate():
         y = int(s[2])
     #make the operation
     if s[1] == '+':
-        var.set(x + y)
+        var.set('{:,}'.format(x + y))
     elif s[1] == '-':
-        var.set(x - y)
+        var.set('{:,}'.format(x - y))
     elif s[1] == chr(215):
-        var.set(x * y)
+        var.set('{:,}'.format(x * y))
     else:
-        var.set(x / y if x % y != 0 else x // y)
+        var.set('{:,}'.format(x / y if x % y != 0 else x // y))
 
 
 
@@ -154,59 +171,60 @@ def decimal_point():
 
 #define Buttons
 #numbers
-num_1 = Button(root, text='1', bg='#1a000d', fg='#33ffff', font=('Times', 20), command=one)
-num_2 = Button(root, text='2', bg='#1a000d', fg='#33ffff', font=('Times', 20), command=two)
-num_3 = Button(root, text='3', bg='#1a000d', fg='#33ffff', font=('Times', 20), command=three)
-num_4 = Button(root, text='4', bg='#1a000d', fg='#33ffff', font=('Times', 20), command=four)
-num_5 = Button(root, text='5', bg='#1a000d', fg='#33ffff', font=('Times', 20), command=five)
-num_6 = Button(root, text='6', bg='#1a000d', fg='#33ffff', font=('Times', 20), command=six)
-num_7 = Button(root, text='7', bg='#1a000d', fg='#33ffff', font=('Times', 20), command=seven)
-num_8 = Button(root, text='8', bg='#1a000d', fg='#33ffff', font=('Times', 20), command=eight)
-num_9 = Button(root, text='9', bg='#1a000d', fg='#33ffff', font=('Times', 20), command=nine)
-num_0 = Button(root, text='0', bg='#1a000d', fg='#33ffff', font=('Times', 20), command=zero)
+num_1 = Button(buttonFrame, text='1', width=7, bg='#1a000d', fg='#33ffff', font=('Times', 20), command=one)
+num_2 = Button(buttonFrame, text='2', width=7, bg='#1a000d', fg='#33ffff', font=('Times', 20), command=two)
+num_3 = Button(buttonFrame, text='3', width=7, bg='#1a000d', fg='#33ffff', font=('Times', 20), command=three)
+num_4 = Button(buttonFrame, text='4', bg='#1a000d', fg='#33ffff', font=('Times', 20), command=four)
+num_5 = Button(buttonFrame, text='5', bg='#1a000d', fg='#33ffff', font=('Times', 20), command=five)
+num_6 = Button(buttonFrame, text='6', bg='#1a000d', fg='#33ffff', font=('Times', 20), command=six)
+num_7 = Button(buttonFrame, text='7', bg='#1a000d', fg='#33ffff', font=('Times', 20), command=seven)
+num_8 = Button(buttonFrame, text='8', bg='#1a000d', fg='#33ffff', font=('Times', 20), command=eight)
+num_9 = Button(buttonFrame, text='9', bg='#1a000d', fg='#33ffff', font=('Times', 20), command=nine)
+num_0 = Button(buttonFrame, text='0', bg='#1a000d', fg='#33ffff', font=('Times', 20), command=zero)
 
 #operations
-addition = Button(root, text='+', bg='#1a000d', fg='#33ffff', font=('Times', 20), command=add)
-subtraction = Button(root, text='-', bg='#1a000d', fg='#33ffff', font=('Times', 20), command=subtract)
-multiplication = Button(root, text=chr(215), bg='#1a000d', fg='#33ffff', font=('Times', 20), command=multiply)
-division = Button(root, text=chr(247), bg='#1a000d', fg='#33ffff', font=('Times', 20), command=divide)
-equal = Button(root, text='=', bg='#1a000d', fg='#33ffff', font=('Times', 20), command=calculate)
+addition = Button(buttonFrame, text='+', width=7, bg='#1a000d', fg='#33ffff', font=('Times', 20), command=add)
+subtraction = Button(buttonFrame, text='−', bg='#1a000d', fg='#33ffff', font=('Times', 20), command=subtract)
+multiplication = Button(buttonFrame, text=chr(215), bg='#1a000d', fg='#33ffff', font=('Times', 20), command=multiply)
+division = Button(buttonFrame, text=chr(247), bg='#1a000d', fg='#33ffff', font=('Times', 20), command=divide)
+equal = Button(buttonFrame, text='=', bg='#1a000d', fg='#33ffff', font=('Times', 20), command=calculate)
 
 #functional Buttons
-clear = Button(root, text='C', bg='#1a000d', fg='#33ffff', font=('Times', 20), command=clean)
-period = Button(root, text='.', bg='#1a000d', fg='#33ffff', font=('Times', 20), command=decimal_point)
+clear = Button(buttonFrame, text='C', bg='#1a000d', fg='#33ffff', font=('Times', 20), command=clean)
+period = Button(buttonFrame, text='.', bg='#1a000d', fg='#33ffff', font=('Times', 20), command=decimal_point)
 
 #Packing Buttons into the form
 #Screen of the calculator
 #screenFrame.grid(row=0, column=0, columnspan=4, sticky=W+E)
 #screenLabel.pack()
-screenLabel.grid(row=0, column=0, columnspan=4, sticky=W+E)
+# screenLabel.grid(row=0, column=0, columnspan=4, sticky=W+E)
+screenLabel.pack(fill=X)
 
 #numbers
-num_1.grid(row=1, column=0, sticky=W+E)
-num_2.grid(row=1, column=1, sticky=W+E)
-num_3.grid(row=1, column=2, sticky=W+E)
+num_1.grid(row=0, column=0, sticky=W+E)
+num_2.grid(row=0, column=1, sticky=W+E)
+num_3.grid(row=0, column=2, sticky=W+E)
 
-num_4.grid(row=2, column=0, sticky=W+E)
-num_5.grid(row=2, column=1, sticky=W+E)
-num_6.grid(row=2, column=2, sticky=W+E)
+num_4.grid(row=1, column=0, sticky=W+E)
+num_5.grid(row=1, column=1, sticky=W+E)
+num_6.grid(row=1, column=2, sticky=W+E)
 
-num_7.grid(row=3, column=0, sticky=W+E)
-num_8.grid(row=3, column=1, sticky=W+E)
-num_9.grid(row=3, column=2, sticky=W+E)
+num_7.grid(row=2, column=0, sticky=W+E)
+num_8.grid(row=2, column=1, sticky=W+E)
+num_9.grid(row=2, column=2, sticky=W+E)
 
-num_0.grid(row=4, column=0, columnspan=2, sticky=W+E)
+num_0.grid(row=3, column=0, columnspan=2, sticky=W+E)
 
 #operations
-addition.grid(row=1, column=3, sticky=W+E)
-subtraction.grid(row=2, column=3, sticky=W+E)
-multiplication.grid(row=3, column=3, sticky=W+E)
-division.grid(row=4, column=3, sticky=W+E)
-equal.grid(row=5, column=3, sticky=W+E)
+addition.grid(row=0, column=3, sticky=W+E)
+subtraction.grid(row=1, column=3, sticky=W+E)
+multiplication.grid(row=2, column=3, sticky=W+E)
+division.grid(row=3, column=3, sticky=W+E)
+equal.grid(row=4, column=3, sticky=W+E)
 
 #functional Buttons
-clear.grid(row=5, column=0, columnspan=3, sticky=W+E)
-period.grid(row=4, column=2, sticky=W+E)
+clear.grid(row=4, column=0, columnspan=3, sticky=W+E)
+period.grid(row=3, column=2, sticky=W+E)
 
 
 
